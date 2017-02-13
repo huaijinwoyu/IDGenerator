82020af7ad79c092246ac51b872236bb092b22b2
shihuayong <sheqnishy@163.com>
2017-02-13 10:03:03 +0800

    【Intellij自动提交】【存档】

---
+++ src/java/cn/shenqiz/idgenerator/SnowFlake.java
@@ -0,0 +1,17 @@
+package cn.shenqiz.idgenerator;
+
+/**
+ * Created by 神奇的Z on 2017-2-13.
+ * 描述：Twitter开源的分布式ID生成算法
+ */
+public class SnowFlake implements ShenqiId<Long> {
+
+    public Long generate() {
+        return null;
+    }
+
+    public boolean exists(Long aLong) {
+        return false;
+    }
+
+}
